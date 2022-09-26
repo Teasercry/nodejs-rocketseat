9e98aaebbc5dd9cfaa0804807674d79db12e82be
Renan G. Silva <renan.silva@crmbonus.com>
2022-09-26 16:23:46 -0300

    methods http

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
 
 app.get("/courses", (request, response) => {
+    const query = request.query;
+    console.log(query);
     return response.json(["Curso1","Curso2","Curso3","Curso4",]);
 });
 
 app.post("/courses", (request, response) => {
+    const body = request.body;
+    console.log(body);
     return response.json(["Curso1","Curso2","Curso3","Curso4",]);
 });
 
 app.put("/courses/:id", (request, response) => {
+    const {id} = request.params;
+    console.log(id);
     return response.json(["Curso1","Curso2","Curso3","Curso4",]);
 });
 
